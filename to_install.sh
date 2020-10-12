61c9d8579649931d03acde8c3de0d64f5d89d6b4
Abd ElFattah Ghazy <abdelfattah.ghazy@nana.sa>
2020-10-12 21:55:26 +0200

    init

--- to_install.sh
+++ to_install.sh
@@ -1,4 +1,23 @@
-postman
+yay
+postman -- from website
 kite
 ohmyzsh
+tmux
+yay -S slack-desktop
+yay -S telegram-desktop
+zoom -- from website
+lsd
+pip install git-big-picture
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+docker 
+docker-compose 
+kubectl
 
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker 
+sudo systemctl enable docker
