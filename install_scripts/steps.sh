99caf5f9e4788c07aea0fe8aefcbd4d1cfbe0892
uniqueinx <e.agx2020@gmail.com>
2020-12-17 20:35:48 +0200

    un

--- install_scripts/steps.sh
+++ install_scripts/steps.sh
@@ -1,12 +1,12 @@
 set -e
-#./create_swap.sh
-#./sync_clock.sh
-#./add_locale.sh
-#./host.sh
-#./pass.sh
-#./install_some.sh
-#./install_grub.sh
-#./enable_service.sh
+./create_swap.sh
+./sync_clock.sh
+./add_locale.sh
+./host.sh
+./pass.sh
+./install_some.sh
+./install_grub.sh
+./enable_service.sh
 ./add_user.sh
 echo "Done installing"
 echo "You can reboot"
