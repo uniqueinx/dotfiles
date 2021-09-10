b173980ff5387171935a703fafecf2a2636ccfc0
Abd ElFattah Ghazy <e.agx2020@gmail.com>
2021-09-10 23:00:24 +0200

    Update install_grub.sh

--- install_scripts/install_grub.sh
+++ install_scripts/install_grub.sh
@@ -1,3 +1,3 @@
 echo "installing grub in uefi mode"
-grub-install target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
